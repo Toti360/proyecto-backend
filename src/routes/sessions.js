dbf54b14c3d4a1cef833c6d38e474fa7a5632429
Roxana Abugauch <aburoxana@hotmail.com>
2024-08-22 01:02:02 -0300

    Preentrega Lista

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import UserModel from "../dao/models/user.model.js";
+import CartManager from "../dao/db/cart-manager-db.js";
 import { createHash, isValidPassword } from "../utils/hashbcrypt.js";
 import passport from "passport";
 import jwt from "jsonwebtoken";
@@ -9,83 +10,98 @@ const JWT_SECRET = "coderhouse";
 
 // Ruta de registro
 router.post("/register", async (req, res) => {
-    const { user, password } = req.body;
+const { first_name, last_name, email, password, age, role } = req.body;
 
-    try {
-        // Verificamos si el usuario ya existe
-        const existeUser = await UserModel.findOne({ user });
+try {
+    // Verificamos si el usuario ya existe
+    const existeUser = await UserModel.findOne({ email: email });
 
-        if (existeUser) {
-            return res.status(400).send("El usuario ya existe");
-        }
-
-        // Creamos el nuevo usuario
-        const newUser = new UserModel({
-            user,
-            password: createHash(password)
-        });
-
-        // Lo guardamos
-        await newUser.save();
-
-        // Generamos el token de JWT
-        const token = jwt.sign({ user: newUser.user, role: newUser.role }, JWT_SECRET, { expiresIn: "1h" });
-
-        // Generamos la cookie
-        res.cookie("coderCookieToken", token, {
-            maxAge: 3600000,
-            httpOnly: true // Accesible solo mediante peticiones HTTP
-        });
-
-        res.redirect("/productos");
-
-    } catch (error) {
-        res.status(500).send("Error interno del servidor");
+    if (existeUser) {
+    return res.status(400).json({ error: "El usuario ya existe" });
     }
+
+    // Creamos un nuevo carrito
+    const cartManager = new CartManager();
+    const carrito = await cartManager.crearCarrito();
+
+    // Creamos el nuevo usuario
+    const newUser = new UserModel({
+    first_name,
+    last_name,
+    email,
+    cart_id: carrito._id,
+    password: createHash(password),
+    age,
+    role: role || "usuario",
+    });
+
+    // Lo guardamos
+    await newUser.save();
+
+    // Generamos el token de JWT
+    const token = jwt.sign(
+    { email: newUser.email, role: newUser.role }, JWT_SECRET, { expiresIn: "1h" }
+    );
+
+    // Generamos la cookie
+    res.cookie("coderCookieToken", token, {
+    maxAge: 3600000,
+    httpOnly: true, // Accesible solo mediante peticiones HTTP
+    });
+
+    res.redirect("/productos");
+
+} catch (error) {
+    console.error("Error en el login:", error);
+    res.status(500).json({ error: `Error interno del servidor: ${error.message}` });
+}
 });
 
 // Ruta de login
 router.post("/login", async (req, res) => {
-    const { user, password } = req.body;
+const { email, password } = req.body;
 
-    try {
-        // Buscamos el usuario en MongoDB
-        const userEncontrado = await UserModel.findOne({ user });
+try {
+    // Buscamos el usuario en MongoDB
+    const userEncontrado = await UserModel.findOne({ email });
 
-        // Verificamos si el usuario existe
-        if (!userEncontrado) {
-            return res.status(401).send("Usuario no válido");
-        }
-
-        // Verificamos la contraseña
-        if (!isValidPassword(password, userEncontrado)) {
-            return res.status(401).send("Contraseña incorrecta");
-        }
-
-        // Generamos el token de JWT
-        const token = jwt.sign({ user: userEncontrado.user, role: userEncontrado.role }, JWT_SECRET, { expiresIn: "1h" });
-
-        // Generamos la cookie
-        res.cookie("coderCookieToken", token, {
-            maxAge: 3600000, // 1 hora de vida
-            httpOnly: true // Accesible solo mediante peticiones HTTP
-        });
-
-        res.redirect("/productos");
-
-    } catch (error) {
-        res.status(500).send("Error interno del servidor");
+    // Verificamos si el usuario existe
+    if (!userEncontrado) {
+    return res.status(401).send("Usuario no válido");
     }
+
+    // Verificamos la contraseña
+    if (!isValidPassword(password, userEncontrado)) {
+    return res.status(401).send("Contraseña incorrecta");
+    }
+
+    // Generamos el token de JWT
+    const token = jwt.sign(
+    { email: userEncontrado.email, role: userEncontrado.role }, JWT_SECRET, { expiresIn: "1h" }
+    );
+
+    // Generamos la cookie
+    res.cookie("coderCookieToken", token, {
+      maxAge: 3600000, // 1 hora de vida
+      httpOnly: true, // Accesible solo mediante peticiones HTTP
+    });
+
+    res.redirect("/productos");
+} catch (error) {
+    console.error("Error en el login:", error);
+    res.status(500).json({ error: `Error interno del servidor: ${error.message}` });
+}
 });
 
 // Ruta para obtener la información del usuario actual
 router.get("/productos", passport.authenticate("jwt", { session: false }), (req, res) => {
     if (req.user) {
-        res.render("home", { user: req.user.user });
-    } else { 
-        res.status(401).send("No Autenticado, Token Inválido");
+    res.render("home", { email: req.user.email });
+    } else {
+    res.status(401).send("No Autenticado, Token Inválido");
     }
-});
+    }
+);
 
 // Ruta para cerrar sesión
 router.get("/logout", (req, res) => {
@@ -93,14 +109,14 @@ router.get("/logout", (req, res) => {
     res.redirect("/login");
 });
 
-//Ruta exclusiva para admins: 
+//Ruta exclusiva para admins:
 
-router.get("/admin", passport.authenticate("jwt", {session:false}), (req, res) => {
-    if(req.user.role !== "admin") {
-        return res.status(403).send("Acceso denegado, no eres Admin 😒!!"); 
-    } 
-    res.render("admin"); 
-});
-
+router.get("/admin", passport.authenticate("jwt", { session: false }), (req, res) => {
+    if (req.user.role !== "admin") {
+    return res.status(403).send("Acceso denegado, no eres Admin 😒!!");
+    }
+    res.render("admin");
+    }
+);
 
 export default router;
